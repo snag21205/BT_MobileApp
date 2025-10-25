05c296f35b3f9a157b4235bb13cfaff5d7ae2669
snag21205 <silentsorrow.21205@gmail.com>
2025-10-25 11:42:36 +0700

    third

--- MyApplication/app/src/main/java/com/example/myapplication/BookGridView.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/BookGridView.kt
@@ -1,10 +1,12 @@
 package com.example.myapplication
 
 import android.app.Activity
+import android.content.Intent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 
 class BookGridView (val activity: Activity,val listBook: List<BookModel>) : ArrayAdapter<BookModel>(activity, R.layout.layout_item, listBook) {
@@ -21,9 +23,22 @@ class BookGridView (val activity: Activity,val listBook: List<BookModel>) : Arra
         } else {
             Glide.with(activity).load(listBook[position].urlBook).into(imgBook)
         }
+        val txtBookName = view.findViewById<TextView>(R.id.txtBookName)
+        txtBookName.text = listBook[position].txtBookName
+
+        view.setOnClickListener {
+            val intent = Intent(activity, DetailActivity::class.java)
+            intent.putExtra("bookName", listBook[position].txtBookName)
+            if (listBook[position].imgBook != 0) {
+                intent.putExtra("bookImage", listBook[position].imgBook)
+            } else {
+                intent.putExtra("bookUrl", listBook[position].urlBook)
+            }
+            activity.startActivity(intent)
+        }
+
         return view
-
-
+        
     }
 
 
